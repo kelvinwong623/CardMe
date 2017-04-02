e6e9375abe756c7f5fee6ef5ee9e7a121c7ae1cd
saboodi <rinkema596@gmail.com>
2017-04-01 17:38:52 -0700

    made a contact class and some basic SQLite

--- CardMe/app/src/main/java/lahacks2017/cardme/TitleScreen.java
+++ CardMe/app/src/main/java/lahacks2017/cardme/TitleScreen.java
@@ -2,6 +2,9 @@ package lahacks2017.cardme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class TitleScreen extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class TitleScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Ravi", "9100000000", "ravi@gmail"));
+        db.addContact(new Contact("Srinivas", "9199999999", "sri@ucla"));
+        db.addContact(new Contact("Tommy", "9522222222", "tommy@trojan"));
+        db.addContact(new Contact("Karthik", "9533333333", "karth@gmail"));
+        db.addContact(new Contact("ayyylmaooo", "911", "ayyy@lmao"));
+
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();
+
+        for (Contact cn : contacts) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName()
+                    + " ,Phone: " + cn.getPhoneNumber() + " ,email: " + cn.getEmail();
+            // Writing Contacts to log
+            Log.d("Name: ", log);
+            //db.deleteContact(cn);
+        }
     }
 }
