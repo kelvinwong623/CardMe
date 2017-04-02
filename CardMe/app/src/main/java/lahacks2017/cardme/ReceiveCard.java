be59089f89205726f9273f62dade5bd9c85d7272
adrayv <draym001@g.ucla.edu>
2017-04-01 22:00:09 -0700

    Receive Card UI comeplte, without Bluetooth loading screen

--- CardMe/app/src/main/java/lahacks2017/cardme/ReceiveCard.java
+++ CardMe/app/src/main/java/lahacks2017/cardme/ReceiveCard.java
@@ -1,7 +1,10 @@
 package lahacks2017.cardme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ReceiveCard extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ReceiveCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_card);
+    }
+
+    public void makeVisible(View view){
+        //response to button
+        Intent intent = new Intent(this, ReceivedConfirmation.class); // FIXME: landing page is "Visible.class"
+        //do bluetooth stuff
+        startActivity(intent);
+        //Log.d("test","hey");
     }
 }
